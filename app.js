b874844698f9a0e8b03cc94a4b5c2750d38fd69c
Sofia Gadea <slgadea@miuandes.cl>
2021-06-13 23:23:08 -0400

    listas funciones

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ async function app(state, update, view){
     while(true){
     const {model, currentView} = state
     const {title, table} = currentView
-    //console.clear()
+    console.clear()
     
     console.log(title)
     printTable(table)
